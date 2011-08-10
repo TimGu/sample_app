4b9cb3222a57d4bd616cce56545899a7e4281714
Tim Gu <xin.tim.gu@gmail.com>
2011-08-10 13:29:41 -0400

    User signup complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
   resources :users
-  match '/signup', :to => 'users#new'
 	
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
